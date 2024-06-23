cafb52f5d20a15984315b44400139540159bbeee
huyb11 <147602092+huyb11@users.noreply.github.com>
2024-06-23 14:26:06 +0700

    task update

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Testing\Fakes\Fake;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
@@ -17,7 +18,8 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name'=>fake()->sentence(),
+            'is_completed'=>rand(0,1)
         ];
     }
 }
